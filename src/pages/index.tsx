bafe55c62e8ba4b9db55f95f7240164b5803289a
Dan Garcia <daniel.of@gmail.com>
2021-03-23 10:32:33 -0300

    feature(index): manipulating prismic posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,8 @@
+import { ReactElement } from 'react';
+import Prismic from '@prismicio/client';
 import { GetStaticProps } from 'next';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,13 +28,40 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps): ReactElement {
+  console.log(JSON.stringify(postsPagination, null, 2));
+  return <h1>HOME</h1>;
+}
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const { next_page, results } = await prismic.query(
+    [Prismic.predicates.at('document.type', 'posts')],
+    {
+      fetch: ['posts.title', 'posts.subtitle', 'posts.author'],
+      pageSize: 1,
+    }
+  );
+  const formatedPosts = results.map(post => ({
+    uid: post.uid,
+    first_publication_date: format(
+      new Date(post.first_publication_date),
+      'd MMM yyyy',
+      {
+        locale: ptBR,
+      }
+    ),
+    data: {
+      title: post.data.title,
+      subtitle: post.data.subtitle,
+      author: post.data.author,
+    },
+  }));
 
-//   // TODO
-// };
+  return {
+    props: {
+      postsPagination: { next_page, results: formatedPosts },
+    },
+    revalidate: 60 * 60, // 1 hour
+  };
+};
