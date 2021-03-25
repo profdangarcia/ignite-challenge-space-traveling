d32dad8446265b5756f3b8791e8455be0ea035ca
Dan Garcia <daniel.of@gmail.com>
2021-03-25 12:13:02 -0300

    feature(_doc): improving SEO

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,6 +11,8 @@ export default class MyDocument extends Document {
             rel="stylesheet"
           />
           <link rel="shortcut icon" href="/favicon.ico" />
+          <meta property="og:image" content="/cover.png" />
+          <meta name="twitter:image" content="/cover.png" />
         </Head>
         <body>
           <Main />
