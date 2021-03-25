cf93d15574dc135bd23f2147d758787248bf3ce4
Dan Garcia <daniel.of@gmail.com>
2021-03-25 10:49:20 -0300

    fix(comments): fixing component to not render twice

--- src/components/Comments/index.tsx
+++ src/components/Comments/index.tsx
@@ -1,21 +1,21 @@
-export const Comments: React.FC = () => (
-  <section
-    ref={elem => {
-      if (!elem) {
-        return;
-      }
-      const scriptElem = document.createElement('script');
-      scriptElem.src = 'https://utteranc.es/client.js';
-      scriptElem.async = true;
-      scriptElem.crossOrigin = 'anonymous';
-      scriptElem.setAttribute(
-        'repo',
-        'profdangarcia/ignite-challenge-space-traveling'
-      );
-      scriptElem.setAttribute('issue-term', 'pathname');
-      scriptElem.setAttribute('label', 'blog-comment');
-      scriptElem.setAttribute('theme', 'dark-blue');
-      elem.appendChild(scriptElem);
-    }}
-  />
-);
+import { useEffect } from 'react';
+
+export const Comments = (): JSX.Element => {
+  useEffect(() => {
+    const scriptElem = document.createElement('script');
+    const anchor = document.getElementById('inject-comments-for-uterances');
+    scriptElem.src = 'https://utteranc.es/client.js';
+    scriptElem.async = true;
+    scriptElem.crossOrigin = 'anonymous';
+    scriptElem.setAttribute(
+      'repo',
+      'profdangarcia/ignite-challenge-space-traveling'
+    );
+    scriptElem.setAttribute('issue-term', 'pathname');
+    scriptElem.setAttribute('label', 'blog-comment');
+    scriptElem.setAttribute('theme', 'dark-blue');
+    anchor.appendChild(scriptElem);
+  }, []);
+
+  return <div id="inject-comments-for-uterances" />;
+};
